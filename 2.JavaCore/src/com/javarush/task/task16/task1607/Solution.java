652e6028df11b6ef54eb8d4ebe401cd82d9eda81
vovik.tsh <vovik_tsh@rambler.ru>
2019-10-19 18:06:50 +0300

    add solution task1607 in master

--- 2.JavaCore/src/com/javarush/task/task16/task1607/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1607/Solution.java
@@ -24,7 +24,6 @@ public class Solution {
             } else {
                 System.out.println("Waiting for " + horses.get(i).getName());
                 horses.get(i).join();
-                //finishedCount++;
             }
         }
         return finishedCount;
