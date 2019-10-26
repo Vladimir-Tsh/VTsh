feb178b7aca19e459814e318bb81052df5e1c3c9
vovik.tsh <vovik_tsh@rambler.ru>
2019-10-26 17:52:45 +0300

    add volatile in solution task1619

--- 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
@@ -5,7 +5,8 @@ package com.javarush.task.task16.task1619;
 */
 
 public class Solution {
-    public static boolean isRun = true;
+    public static volatile boolean isRun = true;
+
     public static void main(String[] args) throws InterruptedException {
         Thread t = new Thread(new TestThread());
         t.start();
