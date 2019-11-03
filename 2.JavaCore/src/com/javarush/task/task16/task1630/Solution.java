fabc5a637371af7f390dbc38e7f0df1d66125e16
vovik.tsh <vovik_tsh@rambler.ru>
2019-11-03 16:49:27 +0300

    add solution task1630

--- 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
@@ -1,15 +1,25 @@
 package com.javarush.task.task16.task1630;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
     public static String firstFileName;
     public static String secondFileName;
 
     //add your code here - добавьте код тут
+    public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    static {
+        try {
+            firstFileName = reader.readLine();
+            secondFileName = reader.readLine();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) throws InterruptedException {
         systemOutPrintln(firstFileName);
@@ -21,6 +31,7 @@ public class Solution {
         f.setFileName(fileName);
         f.start();
         //add your code here - добавьте код тут
+        f.join();
         System.out.println(f.getFileContent());
     }
 
@@ -36,4 +47,38 @@ public class Solution {
     }
 
     //add your code here - добавьте код тут
+    public static class ReadFileThread extends Thread implements ReadFileInterface {
+        private String fullFileName;
+        private List<String> lines = new ArrayList<String>();
+
+        @Override
+        public void setFileName(String fullFileName) {
+            this.fullFileName = fullFileName;
+        }
+
+        @Override
+        public void run() {
+            String line;
+            try {
+                BufferedReader bis = new BufferedReader(new InputStreamReader(new FileInputStream(fullFileName)));
+                while ((line = bis.readLine()) != null) {
+                    lines.add(line);
+                }
+                bis.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public String getFileContent() {
+            String fileContent = "";
+            for (String str : lines) {
+                fileContent += (str + ' ');
+            }
+            return fileContent;
+        }
+    }
 }
