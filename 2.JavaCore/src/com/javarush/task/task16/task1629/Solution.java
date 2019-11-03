b8864b5e980f6a4d6a3a8439c938c9ce74b0e957
vovik.tsh <vovik_tsh@rambler.ru>
2019-11-03 15:52:44 +0300

    add solution task1629

--- 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
@@ -7,15 +7,40 @@ import java.io.InputStreamReader;
 public class Solution {
     public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         Read3Strings t1 = new Read3Strings();
         Read3Strings t2 = new Read3Strings();
 
         //add your code here - добавьте код тут
+        t1.start();
+        t1.join();
+
+        t2.start();
+        t2.join();
+
+        reader.close();
 
         t1.printResult();
         t2.printResult();
     }
 
     //add your code here - добавьте код тут
+    public static class Read3Strings extends Thread {
+        public String str, str1, str2, str3;
+        @Override
+        public void run() {
+            try {
+                str1 = reader.readLine();
+                str2 = reader.readLine();
+                str3 = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void printResult() {
+            str = str1 + ' ' + str2 + ' ' + str3;
+            System.out.println(str);
+        }
+    }
 }
