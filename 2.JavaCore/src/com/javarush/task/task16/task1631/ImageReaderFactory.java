fcc600ecf92923d6552b4edb5f6e69d780ec0ac6
vovik.tsh <vovik_tsh@rambler.ru>
2019-11-03 19:02:25 +0300

    add solution task1631

--- 2.JavaCore/src/com/javarush/task/task16/task1631/ImageReaderFactory.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/ImageReaderFactory.java
@@ -1,4 +1,21 @@
 package com.javarush.task.task16.task1631;
 
+import com.javarush.task.task16.task1631.common.*;
+
 public class ImageReaderFactory {
+    public static ImageReader getImageReader(ImageTypes imageTypes) {
+        ImageReader imageReader = null;
+        if (imageTypes != null) {
+            if (imageTypes == ImageTypes.BMP) {
+                imageReader = new BmpReader();
+            } else if (imageTypes == ImageTypes.JPG) {
+                imageReader = new JpgReader();
+            } else if (imageTypes == ImageTypes.PNG) {
+                imageReader = new PngReader();
+            }
+        } else {
+            throw new IllegalArgumentException("Неизвестный тип картинки");
+        }
+        return imageReader;
+    }
 }
