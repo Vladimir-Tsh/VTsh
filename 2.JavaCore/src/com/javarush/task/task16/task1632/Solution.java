ed9904d0b0dd3df96ef4fa422dc51f645c56ae1b
vovik.tsh <vovik_tsh@rambler.ru>
2019-11-04 12:01:47 +0300

    add solution task1632

--- 2.JavaCore/src/com/javarush/task/task16/task1632/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1632/Solution.java
@@ -9,6 +9,96 @@ import java.util.List;
 public class Solution {
     public static List<Thread> threads = new ArrayList<>(5);
 
-    public static void main(String[] args) {
+    public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static class Thread1 extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+
+            }
+        }
+    }
+
+    public static class Thread2 extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(60000);
+                } catch (InterruptedException e) {
+                    System.out.println("InterruptedException");
+                }
+            }
+        }
+    }
+
+    public static class Thread3 extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    System.out.println("Ура");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static class Thread4 extends Thread implements Message {
+        @Override
+        public void showWarning() {
+            this.interrupt();
+        }
+
+        @Override
+        public void run() {
+            while (!isInterrupted()) {
+
+            }
+        }
+    }
+
+    public static class Thread5 extends Thread {
+        @Override
+        public void run() {
+            List<Integer> numbers = new ArrayList<>();
+            int sum = 0;
+            String str;
+            boolean isNext = true;
+
+            while (isNext) {
+                try {
+                    str = reader.readLine();
+                    if (str.equals("N")) {
+                        isNext = false;
+                    } else {
+                        numbers.add(Integer.parseInt(str));
+                    }
+                } catch (IOException | NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (!numbers.isEmpty()) {
+                for (int i : numbers) {
+                    sum += i;
+                }
+                System.out.println(sum);
+            }
+        }
+
+    }
+
+    static {
+        threads.add(new Thread1());
+        threads.add(new Thread2());
+        threads.add(new Thread3());
+        threads.add(new Thread4());
+        threads.add(new Thread5());
+    }
+
+    public static void main(String[] args) throws InterruptedException {
     }
 }
