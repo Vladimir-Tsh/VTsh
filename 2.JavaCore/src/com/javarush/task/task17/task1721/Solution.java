c091e184384a1f4e59c800e05647458163ff66dd
vovik.tsh <vovik_tsh@rambler.ru>
2019-12-15 13:37:31 +0300

    add solution task1721

--- 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -1,5 +1,12 @@
 package com.javarush.task.task17.task1721;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,9 +19,31 @@ public class Solution {
     public static List<String> forRemoveLines = new ArrayList<String>();
 
     public static void main(String[] args) {
+        String fileNameOne, fileNameTwo;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            fileNameOne = reader.readLine();
+            fileNameTwo = reader.readLine();
+
+            allLines = Files.readAllLines(Paths.get(fileNameOne));
+            forRemoveLines = Files.readAllLines(Paths.get(fileNameTwo));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            new Solution().joinData();
+        } catch (CorruptedDataException e) {
+            e.printStackTrace();
+        }
     }
 
     public void joinData() throws CorruptedDataException {
-
+        if (allLines.containsAll(forRemoveLines)) {
+            allLines.removeAll(forRemoveLines);
+        } else {
+            allLines.clear();
+            throw new CorruptedDataException();
+        }
     }
 }
