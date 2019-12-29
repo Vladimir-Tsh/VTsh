23a08975bd59680617f2632564c70a89ced04dc5
vovik.tsh <vovik_tsh@rambler.ru>
2019-12-29 12:33:13 +0300

    add solution task1802

--- 2.JavaCore/src/com/javarush/task/task18/task1802/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1802/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task18.task1802;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 /* 
 Минимальный байт
@@ -8,5 +10,20 @@ import java.io.FileInputStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        FileInputStream inputStream = new FileInputStream(fileName);
+        int data = 0;
+
+        if (inputStream.available() > 0) {
+            data = inputStream.read();
+            while (inputStream.available() > 0) {
+                int dataTemp = inputStream.read();
+                if (dataTemp < data) data = dataTemp;
+            }
+        }
+        inputStream.close();
+
+        System.out.println(data);
     }
 }
