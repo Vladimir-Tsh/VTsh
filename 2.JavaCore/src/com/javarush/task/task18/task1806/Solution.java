a3b112a47b89ccdf61a8ba6407d3445b9438109c
vovik.tsh <vovik_tsh@rambler.ru>
2020-01-12 11:30:12 +0300

    add solution task1806

--- 2.JavaCore/src/com/javarush/task/task18/task1806/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1806/Solution.java
@@ -10,18 +10,18 @@ import java.io.IOException;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        FileInputStream inputStream = new FileInputStream("c:/data.txt");
+        FileInputStream inputStream = new FileInputStream("D:/Temp/task1806/data.txt");
         // Создаем поток-записи-байт-в-файл
-        FileOutputStream outputStream = new FileOutputStream("c:/result.txt");
+        FileOutputStream outputStream = new FileOutputStream("D:/Temp/task1806/result.txt");
 
-        if (inputStream.read() >= 0) {
+        if (inputStream.available() > 0) {
             //читаем весь файл одним куском
             byte[] buffer = new byte[inputStream.available()];
             int count = inputStream.read(buffer);
             outputStream.write(buffer, 0, count);
         }
 
-        inputStream.reset();
-        outputStream.flush();
+        inputStream.close();
+        outputStream.close();
     }
 }
