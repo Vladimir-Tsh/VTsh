b2963f8c12636833d3522a9b5fc78fb4c6b3e012
vovik.tsh <vovik_tsh@rambler.ru>
2020-01-12 11:39:00 +0300

    add open/close FileInputStream in solution task1807

--- 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
@@ -4,7 +4,13 @@ package com.javarush.task.task18.task1807;
 Подсчет запятых
 */
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputStream = new FileInputStream("D:/Temp/task1806/data.txt");
+
+        inputStream.close();
     }
 }
