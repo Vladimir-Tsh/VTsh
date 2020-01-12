74d645d67c535dc47c30a499a30f0398fa105b11
vovik.tsh <vovik_tsh@rambler.ru>
2020-01-12 13:00:06 +0300

    add solution task1808

--- 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
@@ -13,11 +13,25 @@ public class Solution {
         String fileName2 = reader.readLine();
         String fileName3 = reader.readLine();
         FileInputStream inputStream = new FileInputStream(fileName1);
-        FileOutputStream outputStream1 = new FileOutputStream(fileName2);
-        FileOutputStream outputStream2 = new FileOutputStream(fileName3);
+        if (inputStream.available() > 0) {
+            FileOutputStream outputStream1 = new FileOutputStream(fileName2);
+            FileOutputStream outputStream2 = new FileOutputStream(fileName3);
+
+            int inputSize = inputStream.available();
+            int buffSize = inputSize / 2 + inputSize % 2;
+
+            byte[] outBuff = new byte[buffSize];
+
+            inputStream.read(outBuff);
+            outputStream1.write(outBuff);
+
+            inputStream.read(outBuff);
+            outputStream2.write(outBuff,0, buffSize - inputSize % 2);
+
+            outputStream1.close();
+            outputStream2.close();
+        }
 
         inputStream.close();
-        outputStream1.close();
-        outputStream2.close();
     }
 }
