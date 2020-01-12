64460ef65a73e3e11c69da7c5e77e87853572818
vovik.tsh <vovik_tsh@rambler.ru>
2020-01-12 11:48:56 +0300

    add solution task1807

--- 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
@@ -4,13 +4,27 @@ package com.javarush.task.task18.task1807;
 Подсчет запятых
 */
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        FileInputStream inputStream = new FileInputStream("D:/Temp/task1806/data.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        FileInputStream inputStream = new FileInputStream(fileName);
+
+        int countCommas = 0;
+
+        while (inputStream.available() > 0) {
+            if (inputStream.read() == (int) ',') {
+                countCommas++;
+            }
+        }
 
         inputStream.close();
+
+        System.out.println(countCommas);
     }
 }
