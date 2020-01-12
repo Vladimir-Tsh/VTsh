95a334d47426087b41250b3d118ee1cac278d209
vovik.tsh <vovik_tsh@rambler.ru>
2020-01-12 13:39:03 +0300

    add solution task1810

--- 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
@@ -4,9 +4,22 @@ package com.javarush.task.task18.task1810;
 DownloadException
 */
 
-public class Solution {
-    public static void main(String[] args) throws DownloadException {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws DownloadException, IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean next = true;
+        while (next) {
+            String fileName = reader.readLine();
+
+            FileInputStream inputStream = new FileInputStream(fileName);
+            if (inputStream.available() < 1000) {
+                inputStream.close();
+                throw new DownloadException();
+            }
+            inputStream.close();
+        }
     }
 
     public static class DownloadException extends Exception {
