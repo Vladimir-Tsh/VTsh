6eb693e5819d69d329728776cf56b5b0a93f059c
vovik.tsh <vovik_tsh@rambler.ru>
2020-01-12 13:17:22 +0300

    add solution task1809

--- 2.JavaCore/src/com/javarush/task/task18/task1809/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1809/Solution.java
@@ -4,8 +4,28 @@ package com.javarush.task.task18.task1809;
 Реверс файла
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = reader.readLine();
+        String fileName2 = reader.readLine();
+
+        FileInputStream inputStream = new FileInputStream(fileName1);
+        FileOutputStream outputStream = new FileOutputStream(fileName2);
+
+        int buffSize = inputStream.available();
+
+        byte[] outBuff = new byte[buffSize];
+
+        inputStream.read(outBuff);
+
+        for (int i = buffSize - 1; i >= 0; i--) {
+            outputStream.write(outBuff, i, 1);
+        }
+
+        inputStream.close();
+        outputStream.close();
     }
 }
