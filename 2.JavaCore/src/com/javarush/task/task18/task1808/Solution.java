fd41da8cffbdcbae1dfd286a195d64b2db4eee09
vovik.tsh <vovik_tsh@rambler.ru>
2020-01-12 12:09:29 +0300

    add input/output files in solution task1808

--- 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
@@ -4,8 +4,20 @@ package com.javarush.task.task18.task1808;
 Разделение файла
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = reader.readLine();
+        String fileName2 = reader.readLine();
+        String fileName3 = reader.readLine()
+        FileInputStream inputStream = new FileInputStream(fileName1);
+        FileOutputStream outputStream1 = new FileOutputStream(fileName2);
+        FileOutputStream outputStream2 = new FileOutputStream(fileName3);
+
+        inputStream.close();
+        outputStream1.close();
+        outputStream2.close();
     }
 }
