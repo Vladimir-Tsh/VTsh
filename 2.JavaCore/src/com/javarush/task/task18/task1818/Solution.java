9cda29c8bb0b6268e17b22ef61aabac105c21e83
vovik.tsh <vovik_tsh@rambler.ru>
2020-01-18 20:40:17 +0300

    add solution task1818

--- 2.JavaCore/src/com/javarush/task/task18/task1818/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1818/Solution.java
@@ -4,8 +4,36 @@ package com.javarush.task.task18.task1818;
 Два в одном
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = reader.readLine();
+        String fileName2 = reader.readLine();
+        String fileName3 = reader.readLine();
+
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName1, true);
+        FileInputStream fileInputStream1 = new FileInputStream(fileName2);
+        FileInputStream fileInputStream2 = new FileInputStream(fileName3);
+
+        BufferedInputStream bufferedInputStream1 = new BufferedInputStream(fileInputStream1, 200);
+        BufferedInputStream bufferedInputStream2 = new BufferedInputStream(fileInputStream2, 200);
+
+        int i;
+
+        while((i = bufferedInputStream1.read())!= -1){
+            fileOutputStream.write(i);
+        }
+
+        i = 0;
+
+        while((i = bufferedInputStream2.read())!= -1){
+            fileOutputStream.write(i);
+        }
+
+        fileInputStream1.close();
+        fileInputStream2.close();
+        fileOutputStream.close();
     }
 }
