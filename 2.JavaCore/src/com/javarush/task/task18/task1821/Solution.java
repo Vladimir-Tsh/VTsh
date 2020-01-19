d90ea87cc7e4ac22c2cfc0cb73fdd6dadc054cf4
vovik.tsh <vovik_tsh@rambler.ru>
2020-01-19 12:42:58 +0300

    add solution task1821

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -4,8 +4,33 @@ package com.javarush.task.task18.task1821;
 Встречаемость символов
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputStream = new FileInputStream(args[0]);
+        Map<Integer, Integer> readMap = new HashMap<>();
+        while (inputStream.available() > 0)
+        {
+            int dataTemp = inputStream.read();
+            if (readMap.containsKey(dataTemp)) {
+                int tempCount = readMap.get(dataTemp);
+                readMap.replace(dataTemp, ++tempCount);
+            } else {
+                readMap.put(dataTemp, 1);
+            }
+        }
+
+        List<Integer> sortValuesOfMap = new ArrayList<>();
+        sortValuesOfMap.addAll(readMap.keySet());
+        Collections.sort(sortValuesOfMap);
+
+        for (int i : sortValuesOfMap) {
+            System.out.println((char) i + " " + readMap.get(i));
+        }
+
+        inputStream.close();
     }
 }
