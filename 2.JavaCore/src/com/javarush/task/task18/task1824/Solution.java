033734d8b291c8fc2387fde53eb589ae230c243c
vovik.tsh <vovik_tsh@rambler.ru>
2020-02-01 13:23:09 +0300

    add solution task1824

--- 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
@@ -4,7 +4,33 @@ package com.javarush.task.task18.task1824;
 Файлы и исключения
 */
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
+        BufferedReader readerFileName = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = "";
+        List<BufferedReader> fileReaders = new ArrayList<>();
+        boolean readNextFileName = true;
+        while (readNextFileName) {
+            try {
+                fileName = readerFileName.readLine();
+                fileReaders.add(new BufferedReader(new InputStreamReader(new FileInputStream(fileName))));
+            } catch (FileNotFoundException e) {
+                System.out.println(fileName);
+                for (BufferedReader fileReader : fileReaders) {
+                    try {
+                        fileReader.close();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                readNextFileName = false;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
