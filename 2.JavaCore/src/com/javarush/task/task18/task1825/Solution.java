19485ae28be10012817f98e3f8686a506fc1633f
vovik.tsh <vovik_tsh@rambler.ru>
2020-02-02 13:09:34 +0300

    add solution task1825

--- 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
@@ -11,17 +11,27 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String fileName;
-        String resultFile;
+        String resultFile = "";
         Set<String> fileNames = new TreeSet<>();
         while (!((fileName = reader.readLine()).equals("end"))) {
             fileNames.add(fileName);
+            resultFile = fileName.substring(0, fileName.lastIndexOf(".part"));
         }
 
-        BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream());
+        BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(resultFile));
+        int i;
         for (String fName :
                 fileNames) {
 //            System.out.println(fName);
+            BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(fName));
+            while ((i = inputStream.read()) != -1) {
+                outputStream.write(i);
+            }
+            inputStream.close();
         }
+        outputStream.close();
+
+//        System.out.println(resultFile);
 
     }
 }
