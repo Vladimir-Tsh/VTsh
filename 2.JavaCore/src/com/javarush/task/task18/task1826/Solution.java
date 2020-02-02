9c1cdfe7a0386b738b7ea459665fae4bbc6d7c9f
vovik.tsh <vovik_tsh@rambler.ru>
2020-02-02 13:52:28 +0300

    add solution task1826

--- 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
@@ -4,9 +4,27 @@ package com.javarush.task.task18.task1826;
 Шифровка
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(args[1]));
+        BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(args[2]));
+        int i;
 
+        switch (args[0]) {
+            case "-e":
+                while ((i = inputStream.read()) != -1) {
+                    outputStream.write(i + 1);
+                }
+                break;
+            case "-d":
+                while ((i = inputStream.read()) != -1) {
+                    outputStream.write(i - 1);
+                }
+                break;
+        }
+        inputStream.close();
+        outputStream.close();
     }
-
 }
