5a477c6947b2d563314a6ea6da51d9fe98168ffc
vovik.tsh <vovik_tsh@rambler.ru>
2020-03-01 13:11:40 +0300

    add solution task1906

--- 2.JavaCore/src/com/javarush/task/task19/task1906/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1906/Solution.java
@@ -4,7 +4,25 @@ package com.javarush.task.task19.task1906;
 Четные символы
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String inFileName = reader.readLine();
+        String outFileName = reader.readLine();
+        reader.close();
+        FileReader fileReader = new FileReader(inFileName);
+        FileWriter fileWriter = new FileWriter(outFileName);
+        int i = 0;
+        while (fileReader.ready()) {
+            i++;
+            int data = fileReader.read();
+            if (i%2 == 0) {
+                fileWriter.write(data);
+            }
+        }
+        fileReader.close();
+        fileWriter.close();
     }
 }
