376a834116eca80e826ae506bb068ea9772a218e
vovik.tsh <vovik_tsh@rambler.ru>
2020-03-01 16:06:59 +0300

    add solution task1908

--- 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
@@ -4,7 +4,30 @@ package com.javarush.task.task19.task1908;
 Выделяем числа
 */
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileNameIn = reader.readLine();
+        String fileNameOut = reader.readLine();
+        reader.close();
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileNameIn));
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileNameOut));
+        Pattern pattern = Pattern.compile("\\p{Punct}|\\s|\\n");
+        while (fileReader.ready()) {
+            String line =  fileReader.readLine();
+            ArrayList<String> words = new ArrayList<>(Arrays.asList(pattern.split(line)));
+            for (String word : words) {
+                if (Pattern.matches("\\d+", word)) {
+                    fileWriter.write(word + " ");
+                }
+            }
+        }
+        fileReader.close();
+        fileWriter.close();
     }
 }
