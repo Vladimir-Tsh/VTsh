6341de7e61b3e919edce61a181042a21c9bbc0a2
vovik.tsh <vovik_tsh@rambler.ru>
2020-03-09 15:15:21 +0300

    add solution task1913

--- 2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
@@ -4,10 +4,38 @@ package com.javarush.task.task19.task1913;
 Выводим только цифры
 */
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+
+        //Создаем динамический массив
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        //создаем адаптер к классу PrintStream
+        PrintStream stream = new PrintStream(outputStream);
+        //Устанавливаем его как текущий System.out
+        System.setOut(stream);
+
+        //Вызываем функцию, которая ничего не знает о наших манипуляциях
+        testString.printSomething();
+
+        //Преобразовываем записанные в наш ByteArray данные в строку
+        String result = outputStream.toString();
+
+        //Возвращаем все как было
+        System.setOut(consoleStream);
+
+        //выводим ее в консоль
+        Pattern pattern = Pattern.compile("\\D");
+        Matcher matcher = pattern.matcher(result);
+        result = matcher.replaceAll("");
+        System.out.println(result);
     }
 
     public static class TestString {
