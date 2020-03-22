5eee43c753147a912989082fe260cf69721b8e01
vovik.tsh <vovik_tsh@rambler.ru>
2020-03-22 17:11:29 +0300

    add some change 1 task1919

--- 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
@@ -4,7 +4,22 @@ package com.javarush.task.task19.task1919;
 Считаем зарплаты
 */
 
+import java.sql.SQLOutput;
+
 public class Solution {
     public static void main(String[] args) {
+        String file = null;
+        try {
+            file = args[0];
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Не задано имя файла");
+            e.printStackTrace();
+            System.out.println(e);
+        }
+        finally {
+            System.out.println(file);
+        }
+
     }
 }
