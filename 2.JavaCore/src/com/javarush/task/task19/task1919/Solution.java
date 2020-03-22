43f9504c8b67aef7f7b715f27a12e335c452d87c
vovik.tsh <vovik_tsh@rambler.ru>
2020-03-22 17:31:54 +0300

    add some change 2 task1919

--- 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
@@ -4,22 +4,27 @@ package com.javarush.task.task19.task1919;
 Считаем зарплаты
 */
 
-import java.sql.SQLOutput;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
-    public static void main(String[] args) {
-        String file = null;
-        try {
-            file = args[0];
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Не задано имя файла");
-            e.printStackTrace();
-            System.out.println(e);
-        }
-        finally {
-            System.out.println(file);
-        }
+    public static void main(String[] args) throws IOException {
+        String file = args[0];
+//        System.out.println(file);
 
+        BufferedReader fileReader = new BufferedReader(new FileReader(file));
+
+        Map<String, Double> zarplata = new HashMap<>();
+        String line;
+        String name;
+        Double value;
+        while (fileReader.ready()) {
+            line = fileReader.readLine();
+            name = line.substring(0, line.indexOf(" "));
+            value = Double.valueOf(line.substring(line.indexOf(" ") + 1));
+            System.out.println(name + " : " + value);
+        }
+        fileReader.close();
     }
 }
