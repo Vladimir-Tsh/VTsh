b4b508f63b60c123870adcf5ea5df2418ad2d051
vovik.tsh <vovik_tsh@rambler.ru>
2020-03-22 17:45:16 +0300

    add some change 3 task1919

--- 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
@@ -5,17 +5,16 @@ package com.javarush.task.task19.task1919;
 */
 
 import java.io.*;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         String file = args[0];
-//        System.out.println(file);
 
         BufferedReader fileReader = new BufferedReader(new FileReader(file));
 
-        Map<String, Double> zarplata = new HashMap<>();
+        Map<String, Double> zarplata = new TreeMap<>();
         String line;
         String name;
         Double value;
@@ -23,8 +22,17 @@ public class Solution {
             line = fileReader.readLine();
             name = line.substring(0, line.indexOf(" "));
             value = Double.valueOf(line.substring(line.indexOf(" ") + 1));
-            System.out.println(name + " : " + value);
+            if (zarplata.containsKey(name)) {
+                zarplata.put(name, value + zarplata.get(name));
+            } else {
+                zarplata.put(name, value);
+            }
         }
+
         fileReader.close();
+
+        for (Map.Entry<String, Double> z: zarplata.entrySet()) {
+            System.out.println(z.getKey() + " " + z.getValue());
+        }
     }
 }
