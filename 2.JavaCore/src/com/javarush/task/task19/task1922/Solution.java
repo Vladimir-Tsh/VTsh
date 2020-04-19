271a659da14caa995315c54ad4a1d5c308a3c6ef
vovik.tsh <vovik_tsh@rambler.ru>
2020-04-19 12:53:06 +0300

    add solution task1922

--- 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task19.task1922;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /* 
 Ищем нужные строки
@@ -11,12 +14,39 @@ public class Solution {
     public static List<String> words = new ArrayList<String>();
 
     static {
-        words.add("файл");
-        words.add("вид");
+        words.add("А");
+        words.add("Б");
         words.add("В");
+    }
+
+    public static int countWords(ArrayList<String> strings) {
+        int count = 0;
+        for (String s : strings) {
+            if (words.contains(s)) count++;
+        }
+        return count;
+    }
+
+    public static void strToConsole(String s) {
+        Pattern p = Pattern.compile(" ");
+        if (countWords(new ArrayList<>(Arrays.asList(p.split(s)))) == 2) System.out.println(s);
     }
 
     public static void main(String[] args) {
 
+        BufferedReader reader;
+        String file = "";
+        try {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            file = reader.readLine();
+            reader.close();
+            reader = new BufferedReader(new FileReader(file));
+            while (reader.ready()) {
+                strToConsole(reader.readLine());
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
