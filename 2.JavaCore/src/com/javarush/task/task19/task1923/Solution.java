0d84a3bdad806fbe43c9a71e85fc8e4de38b9dc9
vovik.tsh <vovik_tsh@rambler.ru>
2020-04-26 17:45:57 +0300

    add solution task1923

--- 2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
@@ -4,7 +4,47 @@ package com.javarush.task.task19.task1923;
 Слова с цифрами
 */
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution {
+
+    public static ArrayList<String> arrayWordsToArrayWordsWithDigits(ArrayList<String> words) {
+        ArrayList<String> wordsWithDigits = new ArrayList<>();
+        Pattern containsDigit = Pattern.compile("\\d");
+        for (String word : words) {
+            Matcher matcher = containsDigit.matcher(word);
+            if (matcher.find()) wordsWithDigits.add(word);
+        }
+        return wordsWithDigits;
+    }
+
     public static void main(String[] args) {
+        String file1 = args[0];
+        String file2 = args[1];
+
+        BufferedReader reader;
+        BufferedWriter writer;
+
+        try {
+            reader = new BufferedReader(new FileReader(file1));
+            writer = new BufferedWriter(new FileWriter(file2));
+            Pattern p = Pattern.compile(" ");
+            ArrayList<String> wordsToFile;
+            while (reader.ready()) {
+                String s = reader.readLine();
+                wordsToFile = arrayWordsToArrayWordsWithDigits(new ArrayList<>(Arrays.asList(p.split(s))));
+                for (String word : wordsToFile) {
+                    writer.write(word + ' ');
+                }
+            }
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
