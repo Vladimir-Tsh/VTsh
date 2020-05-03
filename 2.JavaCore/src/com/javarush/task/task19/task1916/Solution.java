a4aea2f3d89e68b389eae41e6fa45f9e489a3790
vovik.tsh <vovik_tsh@rambler.ru>
2020-05-03 19:19:10 +0300

    add start solution task1916

--- 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task19.task1916;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +12,29 @@ public class Solution {
     public static List<LineItem> lines = new ArrayList<LineItem>();
 
     public static void main(String[] args) {
+        BufferedReader reader;
+        String file1, file2;
+        try {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            file1 = reader.readLine();
+            file2 = reader.readLine();
+            reader.close();
+            ArrayList<String> arrayListFromFile1 = fromFileToArrayList(file1);
+            ArrayList<String> arrayListFromFile2 = fromFileToArrayList(file2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    public static ArrayList<String> fromFileToArrayList(String fileName) throws IOException {
+        ArrayList<String> arrayList = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        while (reader.ready()) {
+            arrayList.add(reader.readLine());
+        }
+        reader.close();
+        return arrayList;
+    }
 
     public static enum Type {
         ADDED,        //добавлена новая строка
