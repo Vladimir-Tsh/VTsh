84619e723d056ed3061f2c82eb26990e3b7eef74
vovik.tsh <vovik_tsh@rambler.ru>
2020-05-03 16:05:54 +0300

    add solution task1927

--- 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
@@ -4,10 +4,42 @@ package com.javarush.task.task19.task1927;
 Контекстная реклама
 */
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+
+        //Создаем динамический массив
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        //создаем адаптер к классу PrintStream
+        PrintStream stream = new PrintStream(outputStream);
+        //Устанавливаем его как текущий System.out
+        System.setOut(stream);
+
+        //Вызываем функцию, которая ничего не знает о наших манипуляциях
+        testString.printSomething();
+
+        //Преобразовываем записанные в наш ByteArray данные в строку
+        String result = outputStream.toString();
+
+        //Возвращаем все как было
+        System.setOut(consoleStream);
+
+        //выводим ее в консоль
+        Pattern pattern = Pattern.compile("\n");
+        ArrayList<String> lines = new ArrayList<>(Arrays.asList(pattern.split(result)));
+
+        for (int i = 0; i < lines.size(); i++) {
+            System.out.println(lines.get(i));
+            if (((i + 1) % 2) == 0) System.out.println("JavaRush - курсы Java онлайн");
+        }
     }
 
     public static class TestString {
