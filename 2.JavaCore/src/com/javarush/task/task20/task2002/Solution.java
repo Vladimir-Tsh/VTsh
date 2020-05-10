55502e4e3a1bb600864f164d2357cca7efd23126
vovik.tsh <vovik_tsh@rambler.ru>
2020-05-10 18:08:40 +0300

    add solution task2002

--- 2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
@@ -1,7 +1,13 @@
 package com.javarush.task.task20.task2002;
 
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /* 
 Читаем и пишем в файл: JavaRush
@@ -17,21 +23,58 @@ public class Solution {
 
             JavaRush javaRush = new JavaRush();
             //initialize users field for the javaRush object here - инициализируйте поле users для объекта javaRush тут
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat();
+            User user = new User();
+            user.setBirthDate(new Date());
+            user.setCountry(User.Country.RUSSIA);
+            user.setFirstName("Ivan");
+            user.setLastName("Ivanov");
+            user.setMale(false);
+            javaRush.users.add(user);
+
+            User user1 = new User();
+            user1.setBirthDate(new Date());
+            user1.setCountry(User.Country.UKRAINE);
+            user1.setFirstName("Petr");
+            user1.setLastName("Petrov");
+            user1.setMale(true);
+            javaRush.users.add(user1);
+
             javaRush.save(outputStream);
             outputStream.flush();
 
             JavaRush loadedObject = new JavaRush();
             loadedObject.load(inputStream);
             //here check that the javaRush object is equal to the loadedObject object - проверьте тут, что javaRush и loadedObject равны
+            String lineSeparator = ":";
+            for (User userJR : javaRush.users) {
+                System.out.println(userJR.getFirstName() + lineSeparator +
+                                userJR.getLastName() + lineSeparator +
+                                userJR.getBirthDate() + lineSeparator +
+                                userJR.isMale() + lineSeparator +
+                                userJR.getCountry());
+            }
+            System.out.println("<------------------------------->");
+            for (User userLO : loadedObject.users) {
+                System.out.println(userLO.getFirstName() + lineSeparator +
+                        userLO.getLastName() + lineSeparator +
+                        userLO.getBirthDate() + lineSeparator +
+                        userLO.isMale() + lineSeparator +
+                        userLO.getCountry());
+            }
+
+            if (javaRush.equals(loadedObject)) System.out.println("OK!");
+            else System.out.println("Bad...");
 
             outputStream.close();
             inputStream.close();
 
         } catch (IOException e) {
-            //e.printStackTrace();
+            e.printStackTrace();
             System.out.println("Oops, something is wrong with my file");
         } catch (Exception e) {
-            //e.printStackTrace();
+            e.printStackTrace();
             System.out.println("Oops, something is wrong with the save/load method");
         }
     }
@@ -41,10 +84,38 @@ public class Solution {
 
         public void save(OutputStream outputStream) throws Exception {
             //implement this method - реализуйте этот метод
+            String lineSeparator = System.getProperty("line.separator");
+            SimpleDateFormat dateFormat = new SimpleDateFormat();
+            outputStream.write((users.size() + lineSeparator).getBytes());
+            for (User user : users) {
+                outputStream.write((user.getFirstName() + lineSeparator +
+                                    user.getLastName() + lineSeparator +
+                                    user.getBirthDate().getTime() + lineSeparator +
+                                    user.isMale() + lineSeparator +
+                                    user.getCountry() + lineSeparator).getBytes());
+            }
         }
 
         public void load(InputStream inputStream) throws Exception {
             //implement this method - реализуйте этот метод
+            StringBuilder s = new StringBuilder();
+            int i;
+            while ((i = inputStream.read()) != -1)
+                s.append((char) i);
+            Pattern pattern = Pattern.compile(System.getProperty("line.separator"));
+            ArrayList<String> lines = new ArrayList<>(Arrays.asList(pattern.split(s)));
+            SimpleDateFormat dateFormat = new SimpleDateFormat();
+            if (Integer.parseInt(lines.get(0)) > 0) {
+                for (i = 1; i < 5 * Integer.parseInt(lines.get(0)); i += 5) {
+                    User user = new User();
+                    user.setFirstName(lines.get(i));
+                    user.setLastName(lines.get(i + 1));
+                    user.setBirthDate(Date.from(Instant.ofEpochMilli(Long.parseLong(lines.get(i + 2)))));
+                    user.setMale(Boolean.parseBoolean(lines.get(i + 3)));
+                    user.setCountry(User.Country.valueOf(lines.get(i + 4)));
+                    users.add(user);
+                }
+            }
         }
 
         @Override
