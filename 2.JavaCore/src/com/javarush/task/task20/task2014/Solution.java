829c3bbd55f40c91380cb81d1d82dd95ad941b2d
vovik.tsh <vovik_tsh@rambler.ru>
2020-05-16 16:49:27 +0300

    add solution task2014

--- 2.JavaCore/src/com/javarush/task/task20/task2014/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2014/Solution.java
@@ -1,19 +1,48 @@
 package com.javarush.task.task20.task2014;
 
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 /* 
 Serializable Solution
 */
-public class Solution {
+public class Solution implements Serializable {
     public static void main(String[] args) {
-        System.out.println(new Solution(4));
+//        System.out.println(new Solution(4));
+
+        String file = "D:\\Temp\\task2014\\object.dat";
+
+        FileOutputStream fileOutputStream = null;
+        FileInputStream fileInputStream = null;
+        Solution savedObject = null;
+        Solution loadedObject = null;
+        try {
+            fileOutputStream = new FileOutputStream(file);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            savedObject = new Solution(12);
+            objectOutputStream.writeObject(savedObject);
+            objectOutputStream.close();
+            fileOutputStream.close();
+
+            fileInputStream = new FileInputStream(file);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            loadedObject = (Solution) objectInputStream.readObject();
+            objectInputStream.close();
+            fileInputStream.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\"" + Objects.requireNonNull(savedObject).string + "\" == "
+                + "\"" + Objects.requireNonNull(loadedObject).string + "\" : "
+                + Objects.requireNonNull(savedObject).string.equals(Objects.requireNonNull(loadedObject).string));
     }
 
-    private final String pattern = "dd MMMM yyyy, EEEE";
-    private Date currentDate;
-    private int temperature;
+    transient private final String pattern = "dd MMMM yyyy, EEEE";
+    transient private Date currentDate;
+    transient private int temperature;
     String string;
 
     public Solution(int temperature) {
