e014a3524e39056b0cf73150c9c59a5b00bfefc4
vovik.tsh <vovik_tsh@rambler.ru>
2020-05-17 13:53:31 +0300

    add solution task2017

--- 2.JavaCore/src/com/javarush/task/task20/task2017/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2017/Solution.java
@@ -1,16 +1,28 @@
 package com.javarush.task.task20.task2017;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.util.Objects;
 
 /* 
 Десериализация
 */
 public class Solution {
     public A getOriginalObject(ObjectInputStream objectStream) {
-        return null;
+        A a = null;
+        try {
+            Object o = objectStream.readObject();
+            a = o instanceof  A ? (A) o : null;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        finally {
+            return Objects.isNull(a) ? null : a;
+        }
     }
 
-    public class A {
+    public class A implements Serializable {
     }
 
     public class B extends A {
