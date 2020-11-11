8e4b492607d573978c514706e02ccc3a15337276
Vladimir-Tsh <vovik_tsh@rambler.ru>
2020-11-11 15:32:20 +0300

    the end

--- src/main/java/ru/sbrf/schvv/springboottest/TestController.java
+++ src/main/java/ru/sbrf/schvv/springboottest/TestController.java
@@ -5,17 +5,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.regex.PatternSyntaxException;
 
 @RestController
 public class TestController {
     @GetMapping("/now")
     public Today getTodayDate(@RequestParam(value = "format", defaultValue = "HH:mm:ss dd.MM.y") String format) {
-        return new Today(format);
+        Today resp;
+        try {
+            resp = new Today(format);
+        } catch (IllegalArgumentException e) {
+            resp = new Today("HH:mm:ss dd.MM.y");
+        }
+        return resp;
     }
 
     @GetMapping("/test")
     public Date getTestValue() {
-        return new Today("dd.MM.y").getDateNow();
+        return new Today("d MMMM").getDateNow();
     }
 //
 //    @GetMapping("/date")
